f62aca77808991d4ee8d4b814da5be7336f3df97
Jared Gorski <jared.gorski@liferay.com>
2018-05-25 14:13:42 -0700

    Add error handling route for 500 error

---
+++ node/magnet/src/pages/errors.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+
+export const controller = app => {
+  const electricPath = path.join(__dirname, '..', '..', '..', '..', 'electric');
+
+  app.use((err, req, res, next) => {
+    res.status(500);
+    res.sendFile(path.join(electricPath, 'error/server-err.html'));
+  });
+}
